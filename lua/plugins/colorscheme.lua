ad7d50731070a867734a5432fc57fe543d322a0a
Edward Schenk <edward.schenk4@gmail.com>
2025-10-19 18:46:29 -0500

    changed colorscheme organization

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,27 @@
 return {
   {
+    "rebelot/kanagawa.nvim",
+    name = "kanagawa",
+  },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "rose-pine-moon",
+      colorscheme = "kanagawa-wave",
     },
   },
 }
