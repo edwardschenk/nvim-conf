ee76fc123683230b0add78d5191af18f6514971c
Edward Schenk <eschenk@titledata.com>
2025-10-21 10:59:52 -0500

    modified sonokai settings

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -22,6 +22,9 @@ return {
     "sainnhe/sonokai",
     lazy = false,
     priority = 1000,
+    config = function()
+      vim.g.sonokai_enable_italic = true
+    end,
   },
   {
     "LazyVim/LazyVim",
