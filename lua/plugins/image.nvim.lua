b58d926dcefdf90dc88c0161db660d9bb84c435b
Edward Schenk <eschenk@titledata.com>
2026-03-20 08:47:29 -0500

    Added enable/disable toggle for IMage/molten added debounce script for LSP

--- lua/plugins/image.nvim.lua
+++ lua/plugins/image.nvim.lua
@@ -1,4 +1,7 @@
 return {
   "3rd/image.nvim",
+  enabled = function()
+    return os.getenv("NVIM_ENABLE_IMAGE") == "1"
+  end,
   opts = {},
 }
--- lua/plugins/molten.lua
+++ lua/plugins/molten.lua
@@ -1,6 +1,9 @@
 return {
   {
     "benlubas/molten-nvim",
+    enabled = function()
+      return os.getenv("NVIM_ENABLE_IMAGE") == "1"
+    end,
     version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
     dependencies = { "3rd/image.nvim" },
     build = ":UpdateRemotePlugins",
